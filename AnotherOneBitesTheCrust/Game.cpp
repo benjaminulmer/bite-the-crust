f82f9b7f7b86c1646f0601ff92446fa37dd84f56
Duoshot <lawg@GAMES06.cs.ucalgary.ca>
2016-01-26 18:08:17 -0700

    Parting complete
    Ported code over to the framework. Changed a bit of stuff.

--- AnotherOneBitesTheCrust/Game.cpp
+++ AnotherOneBitesTheCrust/Game.cpp
@@ -1,19 +1,70 @@
 #include "Game.h"
-#include <SDL.h>
+
+void fatalError(string errorString)
+{
+	cout << errorString << endl;
+	cout << "Enter any key to quit...";
+	int tmp;
+	cin >> tmp;
+	SDL_Quit();
+	exit(1);
+}
 
 Game::Game(void)
 {
+
+		window = nullptr;
+		screenWidth = 1024;		//pro csgo resolution
+		screenHeight = 768;
+		gameState = GameState::PLAY;
 }
 
 // The entry point of the game
 void Game::run() {
 	// Preload data, initialize subsystems, anything to do before entering the main loop
-	SDL_Window* window = SDL_CreateWindow("AOBtC", 300, 300, 300, 300, 0);
+	initSystems();
 
 	inputEngine = new InputEngine();
 
+
+
 	mainLoop();
 }
+
+void Game::initSystems()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);		//Initialize SDL
+
+	window = SDL_CreateWindow("Another Bites The Crust", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_OPENGL);
+	
+
+	if(window == nullptr)
+	{
+		fatalError("SDL Window could not be created");
+	}
+
+	SDL_GLContext glContext = SDL_GL_CreateContext(window);
+	if(glContext == nullptr)
+	{
+		fatalError("SDL_GL context could not be created");
+	}
+
+
+	glewExperimental = GL_TRUE;
+	GLenum error = glewInit();
+	if(error != GLEW_OK)
+	{
+		fatalError("Could not init GLEW");
+	}
+	
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);		//enable double buffering
+
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);				//blue background
+
+	renderingEngine.init();
+
+}
+
 
 void Game::mainLoop() {
 	// Cap the minimum timestep physics will use
@@ -24,7 +75,7 @@ void Game::mainLoop() {
 	unsigned int oldTimeMs = SDL_GetTicks();
 	
 	// Game loop
-	while (true) {
+	while (gameState!= GameState::EXIT) {
 		processSDLEvents();
 
 		// Update the player and AI cars
@@ -46,8 +97,10 @@ void Game::mainLoop() {
 		simCount = 0;
 
 		// Render
-		renderingEngine->pushEntities();
-		renderingEngine->draw();
+		//renderingEngine->pushEntities();
+		renderingEngine.displayFunc();
+
+		SDL_GL_SwapWindow(window);
 	}
 }
 
@@ -55,23 +108,19 @@ void Game::processSDLEvents() {
 	SDL_Event event;
     while (SDL_PollEvent(&event)) {
 		if (event.type == SDL_QUIT) {
-			quitGame();
+			gameState = GameState::EXIT;
 		}
 		else if (event.type == SDL_CONTROLLERAXISMOTION || event.type == SDL_CONTROLLERBUTTONDOWN) {
 			inputEngine->processControllerEvent(event);
+		}
+		else if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+		{
+			gameState = GameState::EXIT;
 		}
 		else {
 			// other events, do nothing yet
 		}
 	}
-}
-
-void Game::quitGame() {
-	// Delete subsystems 
-	delete inputEngine;
-
-	SDL_Quit();
-	exit(0);
 }
 
 Game::~Game(void)
--- AnotherOneBitesTheCrust/RenderingEngine.h
+++ AnotherOneBitesTheCrust/RenderingEngine.h
@@ -7,7 +7,25 @@
 #include <vector>
 #include "TestEntity.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
+
+
+
+#include <GL\glew.h>
+
+#include "res_path.h"
+#include "ShaderTools.h"
+
+
 using namespace std;
+using glm::vec3;
+using glm::mat4;
+using glm::perspective;
+
+
 
 class RenderingEngine
 {
@@ -17,5 +35,32 @@ public:
 
 	void pushEntities();
 	void draw();
+
+	void init();
+	void displayFunc();
+
+	void generateIDs();
+	void deleteIDs();
+	void setupVAO();
+	void loadBuffer();
+	void loadModelViewMatrix();
+	void loadProjectionMatrix();
+	void setupModelViewProjectionTransform();
+	void reloadMVPUniform();
+
+
+
+	GLuint vaoID;
+	GLuint basicProgramID;
+
+	GLuint vertBufferID;	//VBO for vertices
+	GLuint colorBufferID;	//VBO for colors
+
+	mat4 MVP;
+	mat4 M;
+	mat4 V;
+	mat4 P;
+
+
 };
 

4aaeea1ddc1ac4d409c4c0b6cd146a301cd96cc6
benjamin_ulmer <benjaminulmer55@gmail.com>
2016-03-02 20:29:36 -0700

    Cleaned warnings

--- AnotherOneBitesTheCrust/ContentLoading.cpp
+++ AnotherOneBitesTheCrust/ContentLoading.cpp
@@ -71,7 +71,7 @@ bool loadVehicleData(char* filename, Vehicle* vehicle) {
 		vehicle->tuning.maxHandBrakeTorque = (float)d["maxHandBrakeTorque"].GetDouble();
 	}
 	if (d.HasMember("maxSteerDegrees")) {
-		vehicle->tuning.maxSteerDegrees = (float)d["maxSteerDegrees"].GetInt();
+		vehicle->tuning.maxSteerDegrees = d["maxSteerDegrees"].GetInt();
 	}
 	if (d.HasMember("engineTorque")) {
 		vehicle->tuning.engineTorque = (float)d["engineTorque"].GetDouble();
@@ -434,7 +434,7 @@ bool ContentLoading::loadOBJNonIndexed(
 			temp_vertices.push_back(vertex);
 		//}else if ( strcmp( lineHeader, "vt" ) == 0 ){
 		//	glm::vec2 uv;
-		//	fscanf(file, "%f %f\n", &uv.x, &uv.y );
+		//	fscanf_s(file, "%f %f\n", &uv.x, &uv.y );
 		//	uv.y = -uv.y; // Invert V coordinate since we will only use DDS texture, which are inverted. Remove if you want to use TGA or BMP loaders.
 		//	temp_uvs.push_back(uv);
 		}else if ( strcmp( lineHeader, "vn" ) == 0 ){
@@ -501,8 +501,8 @@ bool ContentLoading::loadOBJ(
 	std::vector<glm::vec2> temp_uvs;
 	std::vector<glm::vec3> temp_normals;
 
-
-	FILE * file = fopen(path, "r");
+	FILE * file; 
+	errno_t err = fopen_s(&file, path, "r");
 	if( file == NULL ){
 		printf("Impossible to open the file ! Are you in the right path ? See Tutorial 1 for details\n");
 		getchar();
@@ -513,7 +513,7 @@ bool ContentLoading::loadOBJ(
 
 		char lineHeader[128];
 		// read the first word of the line
-		int res = fscanf(file, "%s", lineHeader);
+		int res = fscanf_s(file, "%s", lineHeader);
 		if (res == EOF)
 			break; // EOF = End Of File. Quit the loop.
 
@@ -521,21 +521,21 @@ bool ContentLoading::loadOBJ(
 		
 		if ( strcmp( lineHeader, "v" ) == 0 ){
 			glm::vec3 vertex;
-			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z );
+			fscanf_s(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z );
 			temp_vertices.push_back(vertex);
 		}else if ( strcmp( lineHeader, "vt" ) == 0 ){
 			glm::vec2 uv;
-			fscanf(file, "%f %f\n", &uv.x, &uv.y );
+			fscanf_s(file, "%f %f\n", &uv.x, &uv.y );
 			uv.y = -uv.y; // Invert V coordinate since we will only use DDS texture, which are inverted. Remove if you want to use TGA or BMP loaders.
 			temp_uvs.push_back(uv);
 		}else if ( strcmp( lineHeader, "vn" ) == 0 ){
 			glm::vec3 normal;
-			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z );
+			fscanf_s(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z );
 			temp_normals.push_back(normal);
 		}else if ( strcmp( lineHeader, "f" ) == 0 ){
 			std::string vertex1, vertex2, vertex3;
 			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-			int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2] );
+			int matches = fscanf_s(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2] );
 			if (matches != 9){
 				printf("File can't be read by our simple parser :-( Try exporting with other options\n");
 				return false;
@@ -588,10 +588,9 @@ GLuint ContentLoading::loadDDS(const char * imagepath)
 
 	unsigned char header[124];
 
-	FILE *fp; 
- 
 	/* try to open the file */ 
-	fp = fopen(imagepath, "rb"); 
+	FILE* fp;
+	errno_t err = fopen_s(&fp, imagepath, "rb"); 
 	if (fp == NULL){
 		printf("%s could not be opened. Are you in the right directory ? Don't forget to read the FAQ !\n", imagepath); getchar(); 
 		return 0;
